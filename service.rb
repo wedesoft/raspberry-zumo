54b3627c6eb95c3e3ad80707d5751e633dc972a0
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-16 15:16:36 +0100

    Ignore small values

--- service.rb
+++ service.rb
@@ -3,13 +3,23 @@ require_relative 'udp_server'
 
 
 class Service
+  DEADZONE = 2400
   def initialize
-    GPIO.new
+    @gpio = GPIO.new
     @socket = UDPServer.new
   end
 
+  def adapt value
+    if value >= DEADZONE
+      (value - DEADZONE) * 100.0 / (32767 - DEADZONE)
+    else
+      0.0
+    end
+  end
+
   def update
-    @socket.read
+    axes = @socket.read.split(',').collect &:to_i
+    @gpio.update adapt(-axes[1]), adapt(axes[1]), adapt(-axes[0]), adapt(axes[0])
   end
 end
 
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -8,6 +8,8 @@ describe Service do
   before :each do
     allow(GPIO).to receive(:new).and_return gpio
     allow(UDPServer).to receive(:new).and_return udp_server
+    allow(udp_server).to receive(:read).and_return "0,0"
+    allow(gpio).to receive :update
   end
 
   describe :initialize do
@@ -25,6 +27,35 @@ describe Service do
   describe :update do
     it 'should receive UDP packets' do
       expect(udp_server).to receive :read
+      Service.new.update
+    end
+
+    it 'should update the drives' do
+      expect(gpio).to receive(:update).with 0, 0, 0, 0
+      Service.new.update
+    end
+
+    it 'should drive forward' do
+      allow(udp_server).to receive(:read).and_return "-32767,-32767"
+      expect(gpio).to receive(:update).with 100, 0, 100, 0
+      Service.new.update
+    end
+
+    it 'should drive backward' do
+      allow(udp_server).to receive(:read).and_return "32767,32767"
+      expect(gpio).to receive(:update).with 0, 100, 0, 100
+      Service.new.update
+    end
+
+    it 'should drive left' do
+      allow(udp_server).to receive(:read).and_return "32767,-32767"
+      expect(gpio).to receive(:update).with 100, 0, 0, 100
+      Service.new.update
+    end
+
+    it 'should ignore small values' do
+      allow(udp_server).to receive(:read).and_return "#{Service::DEADZONE},#{-Service::DEADZONE}"
+      expect(gpio).to receive(:update).with 0, 0, 0, 0
       Service.new.update
     end
   end
