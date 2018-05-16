307f7f6b9836d4cb92e1618fc55f2c196f387a67
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-16 15:05:06 +0100

    Receive UDP packets

--- service.rb
+++ service.rb
@@ -1,8 +1,22 @@
 require_relative 'gpio'
+require_relative 'udp_server'
 
 
 class Service
   def initialize
     GPIO.new
+    @socket = UDPServer.new
+  end
+
+  def update
+    @socket.read
+  end
+end
+
+
+if __FILE__ == $0
+  Service = Service.new
+  while true
+    service.update
   end
 end
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -2,10 +2,30 @@ require_relative '../service'
 
 
 describe Service do
+  let(:udp_server) { double 'udp_server' }
+  let(:gpio) { double 'gpio' }
+
+  before :each do
+    allow(GPIO).to receive(:new).and_return gpio
+    allow(UDPServer).to receive(:new).and_return udp_server
+  end
+
   describe :initialize do
     it 'should initialize general purpose input/output' do
       expect(GPIO).to receive :new
       Service.new
+    end
+
+    it 'should initialize the UDP server' do
+      expect(UDPServer).to receive :new
+      Service.new
+    end
+  end
+
+  describe :update do
+    it 'should receive UDP packets' do
+      expect(udp_server).to receive :read
+      Service.new.update
     end
   end
 end
