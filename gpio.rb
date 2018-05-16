5dc4cae4280f07c7adcfd5f68acf01d96fab3ea5
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-16 14:52:28 +0100

    GPIO module

--- gpio.rb
+++ gpio.rb
@@ -4,33 +4,37 @@ rescue LoadError
 end
 
 
-PINS = [7, 11, 13, 15]
-FREQ = 500
-RPi::GPIO.set_numbering :board
+class GPIO
+  PINS = [7, 11, 13, 15]
+  FREQ = 400
 
+  def initialize
+    RPi::GPIO.set_numbering :board
+    for pin in PINS
+      RPi::GPIO.setup pin, as: :output, initialize: :low
+    end
+    @pwms = PINS.collect do |pin|
+      pwm = RPi::GPIO::PWM.new pin, FREQ
+      pwm.start 0
+    end
+  end
 
-for pin in PINS
-  RPi::GPIO.setup pin, as: :output, initialize: :low
+  def update *values
+    @pwms.zip(values).each do |pwm, value|
+      pwm.duty_cycle = value
+    end
+  end
+
+  def stop
+    pwms.collect { |pwm| pwm.stop }
+    RPi::GPIO.reset
+  end
 end
 
-pwms = PINS.collect do |pin|
-  pwm = RPi::GPIO::PWM.new pin, FREQ
-  pwm.start 0
-end
+
+
 
 #pins = [7, 13]  # forward
 #pins = [11, 15] # backward
 #pins = [7, 15]  # left
 #pins = [11, 13] # right
-a, b = 0, 2
-percent = 100
-pwms[a].duty_cycle = percent * 0.7
-pwms[b].duty_cycle = percent
-#sleep 2
-
-pwms[a].duty_cycle = 0.0
-pwms[b].duty_cycle = 0.0
-
-pwms.collect { |pwm| pwm.stop }
-
-RPi::GPIO.reset
