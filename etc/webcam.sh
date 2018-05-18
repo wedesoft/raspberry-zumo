e83f1f85dc7561fe127bee2c93155f233003feae
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-18 20:29:41 +0100

    Rotation

--- etc/webcam.sh
+++ etc/webcam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -s 640x480 -f video4linux2 -r 5 -i /dev/video0 -vf 'transpose=dir=clock,transpose=dir=clock' http://localhost:8090/feed1.ffm
+ffmpeg -s 640x480 -f video4linux2 -r 5 -i /dev/video0 -metadata:s:v rotate=0 -vf 'transpose=dir=clock,transpose=dir=clock' http://localhost:8090/feed1.ffm
