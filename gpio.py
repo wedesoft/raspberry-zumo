bf8035eebbcc00fe7ce6657d4ff06a5d20916fd2
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-28 21:46:11 +0100

    Python GPIO

---
+++ gpio.py
@@ -0,0 +1,18 @@
+import RPi.GPIO
+
+
+class GPIO:
+    PINS = [7, 11, 13, 15]
+    FREQ = 800
+
+    def __init__(self):
+        RPi.GPIO.setmode(GPIO.BOARD)
+        for pin in self.PINS:
+            RPi.GPIO.setup(pin, RPi.GPIO.OUT)
+        self.pwms = [RPi.GPIO.PWM(pin, self.FREQ) for pin in self.PINS]
+        for pwm in self.pwms:
+            pwm.start(0)
+
+    def update(*values):
+        for pwm, value in self.pwms.zip(values):
+            pwm.ChangeDutyCycle(value)
