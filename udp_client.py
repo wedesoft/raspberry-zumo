354fb4f7915279270d3fc62f8dae47b4ee377509
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-28 21:30:45 +0100

    UDP communication in Python

---
+++ udp_client.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+
+
+class UDPClient:
+    def __init__(self, host = 'raspberrypi.local', port = 2200):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.host = host
+        self.port = port
+
+    def write(self, text):
+        self.socket.sendto(text, (self.host, self.port))
+
+
+if __name__ == "__main__":
+    client = UDPClient('', 2200)
+    while True:
+        text = sys.stdin.readline().rstrip()
+        if not text:
+            break
+        client.write(text)
---
+++ udp_server.py
@@ -0,0 +1,17 @@
+import socket
+
+
+class UDPServer:
+    def __init__(self, host = 'raspberrypi.local', port = 2200):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((host, port))
+
+    def read(self):
+        reply, addr = self.socket.recvfrom(20)
+        return reply
+
+
+if __name__ == "__main__":
+    server = UDPServer('', 2200)
+    while True:
+        print(server.read())
