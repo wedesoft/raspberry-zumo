123e73ff1be52bd92c641601e6e11fbf5054a600
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-29 20:16:56 +0100

    *_pwm -> *_drive

--- client.py
+++ client.py
@@ -15,6 +15,6 @@ class Client:
 
     def update(self):
         self.joystick.update()
-        left_pwm = self.adapt(self.joystick.axis.get(1, 0))
-        right_pwm = self.adapt(self.joystick.axis.get(4, 0))
-        self.udp_client.write("%.2f,%.2f" % (left_pwm, right_pwm))
+        left_drive  = self.adapt(self.joystick.axis.get(1, 0))
+        right_drive = self.adapt(self.joystick.axis.get(4, 0))
+        self.udp_client.write("%.2f,%.2f" % (left_drive, right_drive))
