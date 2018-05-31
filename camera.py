6d5001e550a6a38e7df5cfce64f371a5dd10e10e
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-05-31 21:15:11 +0100

    Local variable for resolution

--- camera.py
+++ camera.py
@@ -8,8 +8,9 @@ except ImportError:
 class Camera:
     def __init__(self, resolution=(640, 480)):
         self.camera = PiCamera(resolution=resolution, sensor_mode=3, framerate=10)
+        self.resolution = resolution
 
     def capture(self):
-        raw_capture = PiRGBArray(self.camera.resolution, size=resolution)
+        raw_capture = PiRGBArray(self.camera, size=self.resolution)
         self.camera.capture(raw_capture, format="bgr")
         return raw_capture.array
