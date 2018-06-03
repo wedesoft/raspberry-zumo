669b085db87d5600de2da1fadaf2e209faab4350
Jan Wedekind <jan@wedesoft.de>
2018-06-03 16:06:20 +0000

    flip image

--- camera.py
+++ camera.py
@@ -9,10 +9,11 @@ except ImportError:
 class Camera:
     def __init__(self, resolution=(320, 240)):
         self.camera = PiCamera(resolution=resolution, framerate=90)
-        #self.camera.iso = 800
+        self.camera.hflip = True
+        self.camera.vflip = True
         self.resolution = resolution
 
     def capture(self):
         raw_capture = PiRGBArray(self.camera, size=self.resolution)
-        self.camera.capture(raw_capture, format="rgb", use_video_port=True)
+        self.camera.capture(raw_capture, format="bgr", use_video_port=True)
         return raw_capture.array
