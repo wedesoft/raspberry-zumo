3129149a3df141827f35ac378b2d1624b8d91ce8
Jan Wedekind <jan@wedesoft.de>
2018-06-03 16:00:25 +0000

    Improve framerate

--- camera.py
+++ camera.py
@@ -8,11 +8,11 @@ except ImportError:
 # http://picamera.readthedocs.io/en/release-1.10/fov.html
 class Camera:
     def __init__(self, resolution=(320, 240)):
-        self.camera = PiCamera(resolution=resolution, sensor_mode=6, framerate=42)
-        self.camera.iso = 800
+        self.camera = PiCamera(resolution=resolution, framerate=90)
+        #self.camera.iso = 800
         self.resolution = resolution
 
     def capture(self):
         raw_capture = PiRGBArray(self.camera, size=self.resolution)
-        self.camera.capture(raw_capture, format="bgr")
+        self.camera.capture(raw_capture, format="rgb", use_video_port=True)
         return raw_capture.array
